ec5086ae7f6702f6925d31e3165fc2dbe210156f
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2011-09-21 15:15:22 +0530

    initial commit

--- restful-web/project.clj
+++
@@ -1,3 +0,0 @@
-(defproject restful-web "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
--- restful-web/test/restful_web/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns restful-web.test.core
-  (:use [restful-web.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
